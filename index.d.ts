fc490bc6b428f0bc68289325892ec0b97d042889
wangchong <wangchong@didachuxing.com>
2018-08-08 15:34:58 +0800

    1.3.1 init

--- index.d.ts
+++ index.d.ts
@@ -2,28 +2,34 @@ declare namespace ThinkSequelize {
   interface SequelizeEmitters {
     // success
     // cb(obj?:any)
-    success(cb:Function):SequelizeEmitters;
-    ok(cb:Function):SequelizeEmitters;
+    success(cb: Function): SequelizeEmitters;
+    ok(cb: Function): SequelizeEmitters;
 
     // error
     // cb(err?:Error)
-    error(cb:Function):SequelizeEmitters;
-    failure(cb:Function):SequelizeEmitters;
-    fail(cb:Function):SequelizeEmitters;
+    error(cb: Function): SequelizeEmitters;
+    failure(cb: Function): SequelizeEmitters;
+    fail(cb: Function): SequelizeEmitters;
 
     // done
     // cb(err?:Error)
-    done(cb:Function):SequelizeEmitters;
-    complete(cb:Function):SequelizeEmitters;
+    done(cb: Function): SequelizeEmitters;
+    complete(cb: Function): SequelizeEmitters;
+  }
+
+  interface IncludeAssociation {
+    source: any;
+    target: any;
+    identifier: string;
   }
 
   interface SequelizeResult {
-    save():SequelizeEmitters;
-    update(...args:any[]):SequelizeEmitters;
-    updateAttributes(data:any,opts?:any):SequelizeEmitters;
-    destroy(...args:any[]):SequelizeEmitters;
-    reload():SequelizeEmitters;
-    decrement(obj:any):SequelizeEmitters;
+    save(): SequelizeEmitters;
+    update(...args: any[]): SequelizeEmitters;
+    updateAttributes(data: any, opts?: any): SequelizeEmitters;
+    destroy(...args: any[]): SequelizeEmitters;
+    reload(): SequelizeEmitters;
+    decrement(obj: any): SequelizeEmitters;
   }
 
   interface SequelizeModel {
@@ -33,23 +39,23 @@ declare namespace ThinkSequelize {
     models: object;
     sequel(name: string): SequelizeModel;
 
-    build(obj:any):SequelizeResult;
-    create(obj:any, opts?:any):SequelizeEmitters;
-    bulkCreate(obj:any[]):SequelizeEmitters
-    find(id:number):SequelizeEmitters;
-    find(opts?:any, qopts?:any):SequelizeEmitters;
-    findAll(opts?:any, qopts?:any):SequelizeEmitters;
-    findOrCreate(where:any, defaults:any, opt?:any):SequelizeEmitters;
-    findAndCountAll(opts:any):SequelizeEmitters;
-    sync(options?:{force?:boolean}):SequelizeEmitters;
-    drop():SequelizeEmitters;
-    count():SequelizeEmitters;
-    max(attr:string):SequelizeEmitters;
-    min(attr:string):SequelizeEmitters;
-    belongsTo(model:SequelizeModel);
-    hasOne(model:SequelizeModel, opts?:any);
-    hasMany(model:SequelizeModel, opts?:any);
-    update(obj:any, where:any, opts?:any):SequelizeEmitters;
+    build(obj: any): SequelizeResult;
+    create(obj: any, opts?: any): SequelizeEmitters;
+    bulkCreate(obj: any[]): SequelizeEmitters
+    find(id: number): SequelizeEmitters;
+    find(opts?: any, qopts?: any): SequelizeEmitters;
+    findAll(opts?: any, qopts?: any): SequelizeEmitters;
+    findOrCreate(where: any, defaults: any, opt?: any): SequelizeEmitters;
+    findAndCountAll(opts: any): SequelizeEmitters;
+    sync(options?: { force?: boolean }): SequelizeEmitters;
+    drop(): SequelizeEmitters;
+    count(): SequelizeEmitters;
+    max(attr: string): SequelizeEmitters;
+    min(attr: string): SequelizeEmitters;
+    belongsTo(model: SequelizeModel):IncludeAssociation;
+    hasOne(model: SequelizeModel, opts?: any):IncludeAssociation;
+    hasMany(model: SequelizeModel, opts?: any):IncludeAssociation;
+    update(...args: any[]): SequelizeEmitters;
   }
 
   interface ModelThinkExtend {
@@ -62,10 +68,14 @@ declare namespace ThinkSequelize {
 }
 
 declare module 'thinkjs' {
-  interface Think extends ThinkSequelize.ModelExtend, ThinkSequelize.ModelThinkExtend, ThinkSequelize.SequelizeEmitters, ThinkSequelize.SequelizeResult, ThinkSequelize.SequelizeModel {}
-  interface Controller extends ThinkSequelize.ModelExtend, ThinkSequelize.ModelThinkExtend, ThinkSequelize.SequelizeEmitters, ThinkSequelize.SequelizeResult, ThinkSequelize.SequelizeModel {}
-  interface Context extends ThinkSequelize.ModelExtend, ThinkSequelize.ModelThinkExtend, ThinkSequelize.SequelizeEmitters, ThinkSequelize.SequelizeResult, ThinkSequelize.SequelizeModel {}
-  interface Service extends ThinkSequelize.ModelExtend, ThinkSequelize.ModelThinkExtend, ThinkSequelize.SequelizeEmitters, ThinkSequelize.SequelizeResult, ThinkSequelize.SequelizeModel {}
+  interface Think extends ThinkSequelize.ModelExtend, ThinkSequelize.ModelThinkExtend {
+  }
+  interface Controller extends ThinkSequelize.ModelExtend, ThinkSequelize.ModelThinkExtend {
+  }
+  interface Context extends ThinkSequelize.ModelExtend, ThinkSequelize.ModelThinkExtend {
+  }
+  interface Service extends ThinkSequelize.ModelExtend, ThinkSequelize.ModelThinkExtend {
+  }
 }
 
 export = ThinkSequelize;
